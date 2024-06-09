a69d3bfe5d8334170bb18a0152c93489945a1701
Kim Hee Chan <a01049984394k@gmail.com>
2024-06-09 23:21:14 +0900

    feat : 로지스틱회귀 추가 및 시간 표시
    240609 로지스틱 회귀 js 추가 및 시간표시법 변경

--- functions.js
+++ functions.js
@@ -1,13 +1,23 @@
 // 근무시간 증가함수
 function workingCount_Increase() {
     workingCount++;
-    $('.working-time-frame .working').text(workingCount + "초");
+
+    let Hour_workingCount = parseInt(workingCount / 60 / 60);
+    let Minute_workingCount = parseInt((workingCount % 3600) / 60);
+    let Second_workingCount = workingCount % 60;
+    
+    $('.working-time-frame .working').text(Hour_workingCount + "시 " + Minute_workingCount + "분 " + Second_workingCount + "초");
 }
 
 //휴식시간 증가함수
 function restingCount_Increase() {
     restingCount++;
-    $('.resting-time-frame .resting').text(restingCount + "초");
+
+    let Hour_restingCount = parseInt(restingCount / 60 / 60);
+    let Minute_restingCount = parseInt((restingCount % 3600) / 60);
+    let Second_restingCount = restingCount % 60;
+    
+    $('.resting-time-frame .resting').text(Hour_restingCount + "시 " + Minute_restingCount + "분 " + Second_restingCount + "초");
 }
 
 //물 섭취 증가 함수
@@ -45,7 +55,7 @@ function timeJudgment() {
     let waterDrink = Number(waterDrink_Text.substr(6, 9));
     drinkTime_Per = waterDrink;
 
-    if (drinkTime_Per >= 0.95 && isDrinking == false) {
+    if (drinkTime_Per >= 0.99 && isDrinking == false) {
         isDrinking = true;
         drinkCount_Increase();
 
